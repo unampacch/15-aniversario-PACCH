e1a09572e58e60d89015ae034e6fc2b73fa9afbf
rodrigoaiz <rodrigoaiz@gmail.com>
2024-09-11 13:37:04 -0600

    init

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), icon(
+    {
+    include: {
+      bi: ['facebook', 'threads', 'twitter-x', 'youtube', 'instagram', 'linkedin', 'box-arrow-in-right','envelope-paper-heart','file-earmark-arrow-down', 'box-arrow-in-right', 'link-45deg', 'arrow-bar-right', 'arrow-bar-left']
+    }
+  }
+  )]
+});
---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Celebrando 15 años contigo Portal Académico del CCH';
+export const SITE_DESCRIPTION = 'Para celebrar los 15 años de la creación del Portal Académico del CCH, hemos preparado una serie de actividades que se detallan en esta página.';
