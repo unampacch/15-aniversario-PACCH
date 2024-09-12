da942a0409dff3d1b56aa670a2ffe53b669bd6d7
rodrigoaiz <rodrigoaiz@gmail.com>
2024-09-12 12:12:53 -0600

    seccion eventos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,11 @@ import animations from 'tailwindcss-animated'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+        pinkown: 'rgb(var(--pink-own) / <alpha-value>)'
+      }
+    },
 	},
 	plugins: [
     animations
