f0485635dd7e9e0c906623a930c3e869325e6d4a
rodrigoaiz <rodrigoaiz@gmail.com>
2024-11-26 15:16:59 -0600

    Update Hero component content and styling; add site URL and image to config

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,5 @@
 
 export const SITE_TITLE = 'Celebrando 15 años contigo Portal Académico del CCH';
 export const SITE_DESCRIPTION = 'Para celebrar los 15 años de la creación del Portal Académico del CCH, hemos preparado una serie de actividades que se detallan en esta página.';
+export const SITE_URL = 'https://portalacademico.cch.unam.mx/15aniversario/';
+export const SITE_IMAGE = 'social_img.jpg';
